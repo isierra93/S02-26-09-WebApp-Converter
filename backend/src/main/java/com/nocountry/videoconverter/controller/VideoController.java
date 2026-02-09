121a146402f9443b970b2440dec891a214363018
riki <ricardosk8532@gmail.com>
2026-02-09 14:05:26 -0800

    agregado llamada al service y devolver video

--- backend/src/main/java/com/nocountry/videoconverter/controller/VideoController.java
+++ backend/src/main/java/com/nocountry/videoconverter/controller/VideoController.java
@@ -1,24 +1,28 @@
 package com.nocountry.videoconverter.controller;
 
+import com.nocountry.videoconverter.entities.ConversionJob;
+import com.nocountry.videoconverter.services.VideoJobService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/videos")
 public class VideoController {
 
+    private final VideoJobService videoJobService;
+
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
-    public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> uploadVideo(@RequestParam("file") MultipartFile file) throws Exception {
 
         if (file == null || file.isEmpty()) {
             return ResponseEntity.badRequest().body("No se recibió ningún archivo");
         }
 
-        return ResponseEntity.ok(
-                "Recibido: " + file.getOriginalFilename() +
-                        " | size=" + file.getSize() +
-                        " bytes | type=" + file.getContentType()
-        );
+        ConversionJob video = videoJobService.createJob(file);
+
+        return ResponseEntity.ok(video);
     }
 }
