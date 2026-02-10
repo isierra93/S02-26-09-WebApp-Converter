779c29cb984b46e1d514da1ac4ddc489ec506a9c
Iván Sierra <sierraivan93@gmail.com>
2026-02-10 04:43:05 -0300

    feat: add async CutterService

---
+++ backend/src/main/java/com/nocountry/videoconverter/services/VideoCutterService.java
@@ -0,0 +1,33 @@
+package com.nocountry.videoconverter.services;
+
+import com.nocountry.videoconverter.entities.ConversionJob;
+import com.nocountry.videoconverter.entities.JobStatus;
+import com.nocountry.videoconverter.repositories.ConversionJobRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class VideoCutterService {
+
+    private final ConversionJobRepository conversionJobRepository;
+
+    @Async("asyncCutterExecutor")
+    public CompletableFuture<ConversionJob> conversionJob(ConversionJob conversionJob) throws InterruptedException{
+
+        conversionJob.setStatus(JobStatus.PROCESSING);
+        conversionJobRepository.save(conversionJob);
+
+        // Simulación de demora de 10 segundos - Aca se corta el video con FFpmeg
+        Thread.sleep(10000L);
+        //Si sale mal lanzamos exception, cambiamos el status conversionJob.setStatus(JobStatus.FAILED) y actualiza db
+
+        //Si sale bien, cambiamos Status, guardamos en dB y devolvemos el job terminado
+        conversionJob.setStatus(JobStatus.COMPLETED);
+        conversionJobRepository.save(conversionJob);
+        return CompletableFuture.completedFuture(conversionJob);
+    }
+}
