916219ef6a87aedde5e696cc2e1c2dccd93fb048
Iván Sierra <sierraivan93@gmail.com>
2026-02-12 19:33:13 -0300

    feat: implements rest globalhandler

---
+++ backend/src/main/java/com/nocountry/videoconverter/exceptions/GlobalHandlerException.java
@@ -0,0 +1,58 @@
+package com.nocountry.videoconverter.exceptions;
+
+import com.nocountry.videoconverter.dto.ErrorDto;
+import com.nocountry.videoconverter.exceptions.business.EmptyFileException;
+import com.nocountry.videoconverter.exceptions.business.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleGenericException(Exception e, HttpServletRequest request) {
+        ErrorDto errorDto = new ErrorDto(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                request.getRequestURI(),
+                "Ocurrió un error inesperado en el servidor."
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
+    }
+
+    @ExceptionHandler(EmptyFileException.class)
+    public ResponseEntity<ErrorDto> handleEmptyFileException(EmptyFileException e, HttpServletRequest request) {
+
+        ErrorDto errorDto = new ErrorDto(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                request.getRequestURI(),
+                e.getMessage()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleResourceNotFoundException(
+            ResourceNotFoundException e,
+            HttpServletRequest request) {
+
+        ErrorDto errorDto = new ErrorDto(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                request.getRequestURI(),
+                e.getMessage()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
+    }
+
+}
+
