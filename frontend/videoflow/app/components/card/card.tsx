45c7e526ddb9077fe6076d60a9c70bcf1c646d11
Hector Duarte <hectorduarte240.hd@gmail.com>
2026-02-15 18:12:15 -0300

    feat: agregar sistema de círculos concéntricos con hover en tarjetas y nuevo iconcard1.svg con gradiente lavanda

--- frontend/videoflow/app/components/card/card.tsx
+++ frontend/videoflow/app/components/card/card.tsx
@@ -1,13 +1,18 @@
-import { Clock, Send, Zap } from "lucide-react";
+import { Clock, Send } from "lucide-react";
 export default function Card() {
     return (
         <section className="px-4 sm:px-8 md:px-16 lg:px-24 mt-16">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
-                <article className="bg-white rounded-3xl border border-gray-200 shadow-sm p-10 text-center transition-all duration-700 delay-300 hover:translate-y-2 hover:shadow-[0_4px_6px_-2px_rgba(92,66,184,0.5)] hover:bg-[#DEDCFF]">
-                    <div className="mx-auto w-20 h-20 rounded-full bg-[#A5A0FF] flex items-center justify-center">
-                        <Zap className="w-10 h-10 text-white" />
+                <article className="group bg-white rounded-3xl border border-gray-200 shadow-sm p-10 text-center transition-all duration-700 delay-300 hover:translate-y-2 hover:shadow-[0_4px_6px_-2px_rgba(92,66,184,0.5)] hover:bg-[#DEDCFF]">
+                    
+                    <div className="mx-auto w-22 h-22 rounded-full bg-[#E7ECFC] group-hover:bg-[#5e58d4] flex items-center justify-center transition-all duration-700 delay-300 shadow-[0_6px_14px_rgba(165,160,255,0.35)]">
+
+                        <div className="w-16 h-16 rounded-full bg-[#CDD8FB] group-hover:bg-white transition-all duration-700 delay-300 shadow-[0_6px_14px_rgba(165,160,255,0.35)]  flex items-center justify-center">
+                            <img src="/iconcard1.svg" alt="" className="w-[36px] h-[36px]" />
+                        </div>
                         
                     </div>
+
                     <h3 className="mt-6 text-2xl font-bold text-gray-900">
                         Rápido
                     </h3>
@@ -18,33 +23,45 @@ export default function Card() {
                     </p>
                 </article>
 
-                <article className="bg-white rounded-3xl border border-gray-200 shadow-sm p-10 text-center transition-all duration-600 delay-300 hover:translate-y-2 hover:shadow-[0_4px_6px_-2px_rgba(92,66,184,0.5)] hover:bg-[#DEDCFF]">
-                    <div className="mx-auto w-20 h-20 rounded-full bg-[#E8E7FF] flex items-center justify-center">
-                        <Clock className="w-10 h-10 text-[#5C4CE8]" />
+                <article className="group bg-white rounded-3xl border border-gray-200 shadow-sm p-10 text-center transition-all duration-700 delay-300 hover:translate-y-2 hover:shadow-[0_4px_6px_-2px_rgba(92,66,184,0.5)] hover:bg-[#DEDCFF]">
+                    
+                    <div className="mx-auto w-22 h-22 rounded-full bg-[#E7ECFC] group-hover:bg-[#5e58d4] flex items-center justify-center transition-all duration-700 delay-300 shadow-[0_6px_14px_rgba(165,160,255,0.35)]">
+
+                        <div className="w-16 h-16 rounded-full bg-[#CDD8FB] group-hover:bg-white transition-all duration-700 delay-300 shadow-[0_6px_14px_rgba(165,160,255,0.35)]  flex items-center justify-center">
+                            <img src="/iconcard2.svg" alt="" className="w-[36px] h-[36px]" />
+                        </div>
+                        
                     </div>
+
                     <h3 className="mt-6 text-2xl font-bold text-gray-900">
                         Ahorra tiempo
                     </h3>
                     <p className="mt-4 text-base text-gray-700 leading-relaxed">
                         Concentrate en tu negocio mientras
-                        nosotros nos encargamos de la
-                        conversión de video.
+                        nostros nos encargamos de la conversión
+                        de tus videos.
                     </p>
                 </article>
 
-                <article className="bg-white rounded-3xl border border-gray-200 shadow-sm p-10 text-center transition-all duration-700 delay-300 hover:translate-y-2  hover:shadow-[0_4px_6px_-2px_rgba(92,66,184,0.5)] hover:bg-[#DEDCFF]">
-                    <div className="mx-auto w-20 h-20 rounded-full bg-[#E8E7FF] flex items-center justify-center">
-                        <Send className="w-10 h-10 text-[#5C4CE8]" />
+                <article className="group bg-white rounded-3xl border border-gray-200 shadow-sm p-10 text-center transition-all duration-700 delay-300 hover:translate-y-2 hover:shadow-[0_4px_6px_-2px_rgba(92,66,184,0.5)] hover:bg-[#DEDCFF]">
+                    
+                    <div className="mx-auto w-22 h-22 rounded-full bg-[#E7ECFC] group-hover:bg-[#5e58d4] flex items-center justify-center transition-all duration-700 delay-300 shadow-[0_6px_14px_rgba(165,160,255,0.35)]">
+
+                        <div className="w-16 h-16 rounded-full bg-[#CDD8FB] group-hover:bg-white transition-all duration-700 delay-300 shadow-[0_6px_14px_rgba(165,160,255,0.35)]  flex items-center justify-center">
+                            <img src="/iconcard3.svg" alt="" className="w-[36px] h-[36px]" />
+                        </div>
+                        
                     </div>
+
                     <h3 className="mt-6 text-2xl font-bold text-gray-900">
                         Listo para publicar
                     </h3>
                     <p className="mt-4 text-base text-gray-700 leading-relaxed">
-                        Exportá en el formato perfecto para
-                        todas las principales plataformas
-                        sociales.
+                        Exportá tus videos en formato perfecto
+                        para todas las princiopales plataformas de redes sociales.
                     </p>
                 </article>
+
             </div>
         </section>
     );
