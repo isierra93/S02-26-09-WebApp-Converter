099e2de20b855bb00b8326109c491b1b56d4779a
Iván Sierra <sierraivan93@gmail.com>
2026-02-20 05:46:20 -0300

    fix: outputUrl

--- backend/src/main/java/com/nocountry/videoconverter/services/VideoCutterService.java
+++ backend/src/main/java/com/nocountry/videoconverter/services/VideoCutterService.java
@@ -49,7 +49,7 @@ public class VideoCutterService {
             int exitCode = process.waitFor(); //se espera hasta que acabe
             //0 = bien
             if (exitCode == 0) {
-                conversionJob.setOutputUrl("/out/" + outputName);
+                conversionJob.setOutputUrl(outputName);
                 conversionJob.setStatus(JobStatus.COMPLETED);
                 System.out.println("Conversión exitosa: " + outputPath);
             } else {
