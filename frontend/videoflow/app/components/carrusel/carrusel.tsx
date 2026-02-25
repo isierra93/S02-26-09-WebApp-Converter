1e207ca9f0cc4040efd636dcbf870f952f8cf7db
ChristianIvanLedesma <cristuca1234@gmail.com>
2026-02-24 23:02:09 -0300

    style(ui): modificamos el comportamiento del carrusel

--- frontend/videoflow/app/components/carrusel/carrusel.tsx
+++ frontend/videoflow/app/components/carrusel/carrusel.tsx
@@ -7,10 +7,10 @@ type ItemProps = {
 
 export default function Carrusel() {
     return (
-        <section className="max-w-screen bg-[#F2F2F7] py-3 sm:py-3.5 sponsor-container">
-            <div className="animate-track gap-26">
-                
-                <div className="flex gap-28 shrink-0">
+        <section className="sponsor-container w-full overflow-hidden bg-[#F2F2F7] py-3 sm:py-3.5">
+            <div className="animate-track flex gap-26">
+                {/* Grupo 1 */}
+                <div className="flex shrink-0 gap-28">
                     <Item>Descarga HD gratuita</Item>
                     <Item>Sin marca de agua</Item>
                     <Item>Reutiliza contenido</Item>
@@ -21,14 +21,14 @@ export default function Carrusel() {
                     </Item>
                 </div>
 
-                
-                <div className="flex gap-28 shrink-0">
+                {/* Grupo 2 (duplicado para loop infinito) */}
+                <div className="flex shrink-0 gap-28">
                     <Item>Descarga HD gratuita</Item>
                     <Item>Sin marca de agua</Item>
                     <Item>Reutiliza contenido</Item>
                     <Item>Ahorrá tiempo</Item>
                     <Item>
-                        Un video <ArrowRight size={16} />{" "}
+                        Un video <ArrowRight size={16} />
                         Varios shorts
                     </Item>
                 </div>
@@ -41,10 +41,10 @@ function Item({ children }: ItemProps) {
     return (
         <div className="flex items-center gap-2 whitespace-nowrap">
             <Check
-                className="bg-[#433BFF] h-4 w-4 rounded-full text-white p-0.5"
+                className="h-4 w-4 rounded-full bg-[#433BFF] p-0.5 text-white"
                 size={18}
             />
-            <p className="text-sm font-medium text-gray-700 flex items-center gap-1">
+            <p className="flex items-center gap-1 text-sm font-medium text-gray-700">
                 {children}
             </p>
         </div>
