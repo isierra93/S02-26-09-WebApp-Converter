4730cd43cd3bcd49ed39c2b352377e240e7f67f1
ChristianIvanLedesma <cristuca1234@gmail.com>
2026-02-25 19:06:29 -0300

    Correcciones y mejoras en VideoEditor y MediaControls:- Barra de progreso con tooltip y  funcionando- Logo actualizado usando Link para rediccionar a home- Ajustes de estilos en globals.css y UploadCard- Icono de upload agregado

--- frontend/videoflow/app/components/GeneratedVideo/GeneratedVideo.tsx
+++ frontend/videoflow/app/components/GeneratedVideo/GeneratedVideo.tsx
@@ -37,7 +37,6 @@ export default function GeneratedVideo({ onGenerate }: Props) {
         return () => clearInterval(interval);
     }, []);
 
-    
     useEffect(() => {
         if (complete) {
             const timer = setTimeout(() => {
@@ -49,8 +48,8 @@ export default function GeneratedVideo({ onGenerate }: Props) {
     }, [complete, onGenerate]);
 
     return (
-        <div className="mt-[80px] flex w-full justify-center px-[20px] sm:px-[48px] md:px-[156px] lg:px-[364px] xl:px-[364px] 2xl:px-[604px]">
-            <div className="w-full rounded-2xl border-[0.25px] border-gray-300/90 bg-[#F2F2F7] text-center shadow-[0px_4px_17.6px_0px_#0000001A] sm:w-[720px] md:w-[720px]">
+        <div className="mt-20 flex w-full justify-center px-5 sm:px-12 md:px-39 lg:px-91  2xl:px-151">
+            <div className="w-full rounded-2xl border-[0.25px] border-gray-300/90 bg-[#F2F2F7] text-center shadow-[0px_4px_17.6px_0px_#0000001A] sm:w-180 md:w-180">
                 <h3 className="my-8 text-center text-[21px] font-semibold text-[#000000]">
                     Creando tu corto
                 </h3>
@@ -66,7 +65,7 @@ export default function GeneratedVideo({ onGenerate }: Props) {
                     />
                 </div>
 
-                <div className="mx-auto mt-5 mb-15 flex h-auto w-full max-w-[327px] flex-col items-start justify-center gap-3 px-4 text-[15px] sm:px-0 sm:text-[17px]">
+                <div className="mx-auto mt-5 mb-15 flex h-auto w-full max-w-81.75 flex-col items-start justify-center gap-3 px-4 text-[15px] sm:px-0 sm:text-[17px]">
                     {steps.map((text, index) => (
                         <div
                             key={index}
